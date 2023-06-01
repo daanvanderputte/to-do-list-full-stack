a2edd700f2dbf0fa2213ddd63e71cdcd06c35d81
daanvanderputte <daanvanderputte@protonmail.com>
2023-06-01 19:26:48 +0200

    added post and get all routes

--- server/routes/todoRoutes.js
+++ server/routes/todoRoutes.js
@@ -1,1 +1,27 @@
+import express from "express";
+import Todo from "../models/todo.js";
 
+const router = express.Router();
+
+router.post("/new", async (req, res) => {
+  const { text, author } = req.body;
+
+  try {
+    await Todo.create({
+      text,
+      author,
+    });
+
+    res.send("Success");
+  } catch (error) {
+    res.status(500).send("Unable to create entry, " + error.message);
+  }
+});
+
+router.get("/all", async (req, res) => {
+  const todos = await Todo.find();
+
+  return res.status(200).json(todos);
+});
+
+export default router;
